57bd5c3c8c49bf0fac1bca7da096d2c43f331b2b
velkuns <romain.inscription@hotmail.fr>
2018-03-14 18:30:08 +0100

    Kernel console:
     * Change loading cache methods
     * Change loading service container

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,7 +10,7 @@
 namespace Eureka\Kernel\Console;
 
 use Eureka\Component\Config\Config;
-use Eureka\Component\Container\Container;
+use Eureka\Component\Config\ConfigCache;
 
 class Kernel
 {
@@ -32,23 +32,25 @@ class Kernel
     /**
      * Kernel constructor.
      *
-     * @param string $root Root path
-     * @param string $env Environment name
-     * @param bool $debug If is debug mode
-     * @param string $config Config directory
+     * @param  string $root
+     * @param  string $environment
+     * @param  bool $debug
+     * @param  string $configDirectory
+     * @param  string $kernelDirectory
+     * @param  string $servicesDirectory
+     * @throws \Eureka\Component\Config\Exception\ConfigException
      * @throws \Eureka\Component\Config\Exception\InvalidConfigException
-     * @throws \Psr\Container\NotFoundExceptionInterface
+     * @throws \Eureka\Component\Container\Exception\ContainerException
      */
-    public function __construct($root, $env, $debug = false, $config = 'config')
+    public function __construct($root, $environment, $debug = false, $configDirectory = 'config', $kernelDirectory = 'kernel', $servicesDirectory = 'services')
     {
-        $this->env   = $env;
-        $this->debug = $debug;
-        $this->root  = $root;
+        $this->environment = $environment;
+        $this->debug       = $debug;
+        $this->root        = $root;
 
         $this
             ->initDebug(false)
-            ->initConfig($root . DIRECTORY_SEPARATOR . $config)
-            ->initConfigPackages()
+            ->initConfig($configDirectory, $kernelDirectory, $servicesDirectory)
             ->initContainer()
             ->initDebug(true)
         ;
@@ -95,17 +97,67 @@ class Kernel
     /**
      * Load Config path.
      *
-     * @param  string $path
+     * @param  string $configDirectory
+     * @param  string $kernelDirectory
+     * @param  string $servicesDirectory
      * @return $this
+     * @throws \Eureka\Component\Config\Exception\ConfigException
      * @throws \Eureka\Component\Config\Exception\InvalidConfigException
      */
-    protected function initConfig($path)
+    protected function initConfig($configDirectory, $kernelDirectory, $servicesDirectory)
     {
-        $this->config = (new Config($this->env))->loadYamlFromDirectory($path);
-        $this->config->add('kernel.root', $this->root);
-        $this->config->add('kernel.env', $this->env);
-        $this->config->add('kernel.environment', $this->env);
-        $this->config->add('kernel.debug', $this->debug);
+        $this->config = new Config($this->environment);
+        $configCache  = new ConfigCache(
+            $this->root . DIRECTORY_SEPARATOR . $configDirectory . DIRECTORY_SEPARATOR . $kernelDirectory,
+            $this->environment
+        );
+
+        //~ Load config from cache
+        if ($configCache->hasCache() && $configCache->isEnabled()) {
+            $this->config->set($configCache->loadFromCache());
+            return $this;
+        }
+
+        $this
+            ->initConfigKernel($this->root . DIRECTORY_SEPARATOR . $configDirectory . DIRECTORY_SEPARATOR . $kernelDirectory)
+            ->initConfigApplication($this->root . DIRECTORY_SEPARATOR . $configDirectory)
+            ->initConfigPackages()
+            ->initConfigServices($this->root . DIRECTORY_SEPARATOR . $configDirectory, $servicesDirectory)
+        ;
+
+        $configCache->dumpCache($this->config->get());
+
+        return $this;
+    }
+
+    /**
+     * @param  string $path
+     * @return $this
+     * @throws \Eureka\Component\Config\Exception\ConfigException
+     * @throws \Eureka\Component\Config\Exception\InvalidConfigException
+     */
+    protected function initConfigKernel($path)
+    {
+        $this->config
+            ->loadYamlFromDirectory($path)
+            ->add('kernel.root', $this->root)
+            ->add('kernel.env', $this->environment)
+            ->add('kernel.environment', $this->environment)
+            ->add('kernel.debug', $this->debug)
+        ;
+
+        return $this;
+    }
+
+    /**
+     * @param  string $path
+     * @return $this
+     * @throws \Eureka\Component\Config\Exception\ConfigException
+     * @throws \Eureka\Component\Config\Exception\InvalidConfigException
+     */
+    protected function initConfigApplication($path)
+    {
+        $this->config->loadYamlFromDirectory($path);
 
         return $this;
     }
@@ -115,6 +167,7 @@ class Kernel
      *
      * @return $this
      * @throws \Eureka\Component\Config\Exception\InvalidConfigException
+     * @throws \Eureka\Component\Config\Exception\ConfigException
      */
     protected function initConfigPackages()
     {
@@ -129,9 +182,33 @@ class Kernel
                 continue;
             }
 
-            $namespace = empty($data['namespace']) ? 'app.' : $data['namespace'] . '.';
+            $this->config->loadYamlFromDirectory($data['config']);
+        }
 
-            $this->config->loadYamlFromDirectory($data['config'], $namespace);
+        return $this;
+    }
+
+    /**
+     * @param  string $path
+     * @param  string $servicesDirectory
+     * @return $this
+     * @throws \Eureka\Component\Config\Exception\ConfigException
+     * @throws \Eureka\Component\Config\Exception\InvalidConfigException
+     */
+    protected function initConfigServices($path, $servicesDirectory)
+    {
+        $this->config->loadYamlFromDirectory($path . DIRECTORY_SEPARATOR . $servicesDirectory);
+
+        $list = $this->config->get('app.package');
+
+        if (!empty($list) && is_array($list)) {
+            foreach ($list as $name => $data) {
+                if (!isset($data['config'])) {
+                    continue;
+                }
+
+                $this->config->loadYamlFromDirectory($data['config'] . DIRECTORY_SEPARATOR . $servicesDirectory);
+            }
         }
 
         return $this;
@@ -139,11 +216,11 @@ class Kernel
 
     /**
      * @return $this
-     * @throws \Psr\Container\NotFoundExceptionInterface
+     * @throws \Eureka\Component\Container\Exception\ContainerException
      */
     protected function initContainer()
     {
-        $this->container = Container::makeFromArray($this->config->get('app.services'));
+        $this->container = ServicesContainerFactory::makeFromConfig($this->getConfig());
         $this->container->attach('config', $this->getConfig());
 
         return $this;
