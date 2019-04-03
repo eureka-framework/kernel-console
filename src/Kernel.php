f3adcb2dadb36abbc4c2c05bd029cf9a68119fd8
velkuns <romain.inscription@hotmail.fr>
2019-04-03 09:04:31 +0200

    Branch 2.x: Minor improvement
     * Now based on Eurekon 3.0
     * Re-order config loading

--- src/Kernel.php
+++ src/Kernel.php
@@ -127,18 +127,21 @@ class Kernel
         $loader = $this->getContainerLoader($this->container);
 
         $this->container->setParameter('kernel.environment', $this->environment);
+        $this->container->setParameter('kernel.directory.root', $this->rootDirectory);
 
         //~ Load kernel config files
         $loader->load($this->getConfigDir() . '/{kernel}' . self::CONFIG_EXTENSIONS, 'glob');
         $loader->load($this->getConfigDir() . '/{kernel}_' . $this->environment . self::CONFIG_EXTENSIONS, 'glob');
 
+        //~ Load packages config files
+        $loader->load($this->getConfigDir() . '/{packages}/*' . self::CONFIG_EXTENSIONS, 'glob');
+        $loader->load($this->getConfigDir() . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTENSIONS, 'glob');
+
         //~ Load services config files
         $loader->load($this->getConfigDir() . '/{services}' . self::CONFIG_EXTENSIONS, 'glob');
         $loader->load($this->getConfigDir() . '/{services}_' . $this->environment . self::CONFIG_EXTENSIONS, 'glob');
 
-        //~ Load packages config files
-        $loader->load($this->getConfigDir() . '/{packages}/*' . self::CONFIG_EXTENSIONS, 'glob');
-        $loader->load($this->getConfigDir() . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTENSIONS, 'glob');
+        $this->container->setParameter('kernel.directory.root', $this->rootDirectory);
 
         return $this;
     }
